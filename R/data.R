e636b403bc0037a4b37884bb6fff89d61096b7b1
Wanding Zhou <zhouwanding@gmail.com>
2021-04-11 11:10:33 -0400

    update to 4/8/2021 ExperimentHub upload

--- R/data.R
+++ R/data.R
@@ -5,15 +5,15 @@ eh_id_lookup = c(
     ## data.frame(name=eh$title, eh=names(eh))
     "EPIC.1.LNCaP"="EH3659",
     "EPIC.5.normal"="EH3660",
-    "HM450.1.TCGA.PAAD"="EH3661",
+    ## "HM450.1.TCGA.PAAD"="EH3661",
     "HM450.10.TCGA.PAAD.normal"="EH3662",
     "HM450.10.TCGA.BLCA.normal"="EH3663",
     "HM450.76.TCGA.matched"="EH3664",
     "genomeInfo.hg19"="EH3665",
     "genomeInfo.hg38"="EH3666",
-    "EPIC.address"="EH3667",
-    "HM450.address"="EH3668",
-    "HM27.address"="EH3669",
+    ## "EPIC.address"="EH3667",
+    ## "HM450.address"="EH3668",
+    ## "HM27.address"="EH3669",
     "EPIC.hg19.manifest"="EH3670",
     "EPIC.hg38.manifest"="EH3671",
     "HM27.hg19.manifest"="EH3672",
@@ -31,9 +31,21 @@ eh_id_lookup = c(
     "detection.stats"="EH3684",
     ## eh = query(ExperimentHub(localHub=TRUE), c("sesameData", "v1.9.1"))
     ## data.frame(name=eh$title, eh=names(eh))
-    "MM285.address"="EH4678",
-    "Mammal40.address"="EH4679",
-    "MM285.mm10.manifest"="EH4680"
+    ## "MM285.address"="EH4678",
+    ## "Mammal40.address"="EH4679",
+    "MM285.mm10.manifest"="EH4680",
+    "EPIC.address"="EH5963",
+    "HM27.address"="EH5964",
+    "HM450.1.TCGA.PAAD"="EH5965",
+    "HM450.address"="EH5966",
+    "Mammal40.address"="EH5967",
+    "MM285.1.NOD.FrontalLobe"="EH5968",
+    "MM285.10.tissue"="EH5969",
+    "MM285.address"="EH5970",
+    "MM285.clock347"="EH5971",
+    "MM285.mm10.manifest"="EH5972",
+    "MM285.strain.snp.table"="EH5973",
+    "MM285.tissueSignature"="EH5974"
 )
 
 cacheEnv <- new.env()
