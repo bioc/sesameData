f950cc0e6d2bbd65fd9f5e51cc52a0066c7fe356
Wanding Zhou <zhouwanding@gmail.com>
2022-02-05 19:38:35 -0500

    bump to 1.13.19

--- R/manifest.R
+++ R/manifest.R
@@ -33,6 +33,7 @@ sesameData_getManifestDF <- function(platform, genome=NULL, version=1) {
 #'
 #' @param platform Mammal40, MM285, EPIC, and HM450
 #' @param genome hg38, mm10 etc.
+#' @param decoy whether to include probes mapped to decoy sequence
 #' @param version release version, default is the latest
 #' @param columns additional columns to add from the manifest to mcols
 #' @return GRanges
@@ -41,18 +42,26 @@ sesameData_getManifestDF <- function(platform, genome=NULL, version=1) {
 #' gr <- sesameData_getManifestGRanges("Mammal40")
 #' @export
 sesameData_getManifestGRanges <- function(
-    platform, genome=NULL, version=1, columns = c("mapQ_A", "mapAS_A")) {
+    platform, genome = NULL, version = 1, decoy = FALSE,
+    columns = c("mapQ_A", "mapAS_A")) {
     genome <- check_genomes(genome, platform)
     df <- sesameData_getManifestDF(platform, genome=genome, version=version)
 
     chrms <- df$CpG_chrm
     chrms <- chrms[!is.na(chrms)]
     if (genome %in% c("mm10","mm39","hg19","hg38")) {
-        chrms <- guess_chrmorder(chrms)
+        if (decoy) {
+            chrms <- c(
+                guess_chrmorder(chrms[!grepl("_", chrms)]),
+                sort(unique(chrms[grepl("_", chrms)])))
+        } else {
+            chrms <- guess_chrmorder(chrms[!grepl("_", chrms)])
+        }
     } else {
         chrms <- sort(unique(chrms))
     }
     df <- df[!is.na(df$CpG_chrm) & !is.na(df$CpG_beg) & !is.na(df$CpG_end),]
+    df <- df[df$CpG_chrm %in% chrms,]
     gr <- GRanges(df$CpG_chrm,
         IRanges(df$CpG_beg+1, df$CpG_end),
         strand = ifelse(df$mapFlag_A=="0", "+", "-"),
