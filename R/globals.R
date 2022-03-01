32c970f97b9f6437edfc0dcc242e3ad867d448b5
Wanding Zhou <zhouwanding@gmail.com>
2022-03-01 11:35:51 -0500

    bump to 1.13.30, data uploaded

--- R/globals.R
+++ R/globals.R
@@ -2,7 +2,7 @@
 #'
 #' This is an internal object which will be updated on every new release
 #' library(ExperimentHub)
-#' eh <- query(ExperimentHub(localHub=FALSE), c("sesameData", "v1.11.7"))
+#' eh <- query(ExperimentHub(localHub=FALSE), c("sesameData", "v1.13.1"))
 #' data.frame(name=eh$title, eh=names(eh))
 #'
 #' @name df_master
