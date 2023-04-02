1435a2c164d076f85dc52b3c7621e8fa70a81f7f
Wanding Zhou <zhouwanding@gmail.com>
2023-04-02 12:52:43 -0400

    bump to 1.17.7

--- R/globals.R
+++ R/globals.R
@@ -7,10 +7,11 @@
 #'
 #' @name df_master
 #' @docType data
+#' @return master sheet of sesameData objects
 NULL
 
 cacheEnv <- new.env()
-sesameData_open_alt <- TRUE # whether we can access alternative, only in devel
+sesameData_open_alt <- FALSE # whether we can access alternative, only in devel
 alt_base <- "https://zhouserver.research.chop.edu"
 
 #' Check whether the title exists in cacheEnv
